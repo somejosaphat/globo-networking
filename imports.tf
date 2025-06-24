7315e2d94efd481eedbf4bdaf808fc2933010a76
Josaphat Somé <somejosaphat@gmail.com>
2025-06-24 20:52:29 +0200

    Add third subnet and tags

--- imports.tf
+++ imports.tf
@@ -5,46 +5,46 @@
 
 import {
   to = module.main.aws_vpc.this[0]
-  id = "vpc-02b90a42e47c5fb3c" #VPC
+  id = "vpc-0a43656e82825d76e" #VPC
 }
 
 import {
   to = module.main.aws_subnet.public[0]
-  id = "subnet-081e9848449e36d98" #PublicSubnet1
+  id = "subnet-0f96c6b1dc14d66a9" #PublicSubnet1
 }
 
 import {
   to = module.main.aws_subnet.public[1]
-  id = "subnet-0a7e91c46ca9c325f" #PublicSubnet2
+  id = "subnet-04078f974f4b64787" #PublicSubnet2
 }
 
 import {
   to = module.main.aws_internet_gateway.this[0]
-  id = "igw-07a6ee010070cf033" #InternetGateway
+  id = "igw-0c0a539d71ee5faa8" #InternetGateway
 }
 
 import {
   to = module.main.aws_route.public_internet_gateway[0]
-  id = "rtb-0b391df5dc6ea63c1_0.0.0.0/0" #DefaultPublicRoute
+  id = "rtb-0871c06ba15b54bde_0.0.0.0/0" #DefaultPublicRoute
 }
 
 import {
   to = module.main.aws_route_table.public[0]
-  id = "rtb-0b391df5dc6ea63c1" #PublicRouteTable
+  id = "rtb-0871c06ba15b54bde" #PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[0]
-  id = "subnet-081e9848449e36d98/rtb-0b391df5dc6ea63c1" #PublicSubnet1/PublicRouteTable
+  id = "subnet-0f96c6b1dc14d66a9/rtb-0871c06ba15b54bde" #PublicSubnet1/PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[1]
-  id = "subnet-0a7e91c46ca9c325f/rtb-0b391df5dc6ea63c1" #PublicSubnet2/PublicRouteTable
+  id = "subnet-04078f974f4b64787/rtb-0871c06ba15b54bde" #PublicSubnet2/PublicRouteTable
 }
 
 import {
   to = aws_security_group.ingress
-  id = "sg-0a392bc8b0a93c12a" #NoIngressSecurityGroup
+  id = "sg-0e8626eb4b9341950" #NoIngressSecurityGroup
 }
 
